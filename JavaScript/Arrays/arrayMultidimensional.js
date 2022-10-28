62250e29b7a3e9e72697a4676f9b4c9228556b65
João Victor <joaohaeckel@gmail.com>
2022-10-28 11:14:35 -0300

    Update arrayMultidimensional.js

--- JavaScript/Arrays/arrayMultidimensional.js
+++ JavaScript/Arrays/arrayMultidimensional.js
@@ -4,4 +4,4 @@ const notas = [7,8,7];
            //  0        1
 let multi = [alunos, notas];
 
-console.log('A nota de ${multi[0][0]}, foi de ${multi[1][0]}');
+console.log(`A nota de ${multi[0][0]}, foi de ${multi[1][0]}`);
